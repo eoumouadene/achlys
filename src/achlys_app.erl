3bfebc1e3459a04a92007c73b7aeb472ec2226de
eoumouadene <elias.oumouadene@student.uclouvain.be>
2019-12-18 14:54:12 +0100

    working version bad print

--- src/achlys_app.erl
+++ src/achlys_app.erl
@@ -14,9 +14,9 @@
 %% Application callbacks
 -export([start/2 ,
          stop/1,
-         daemon/4,
+         daemon/5,
          declare_loop/3,
-         mean_compute/4,
+         mean_compute/9,
          timestamp/0]).
 
 %%%===================================================================
@@ -50,10 +50,14 @@ start(_StartType , _StartArgs) ->
             {ok, _} = application:ensure_all_started(grisp),
             %LEDs = [1, 2],
             Name = erlang:node(),
-            NumberOfNodes = 1,
+            NumberOfNodes = 2,
             Id = lists:nth(2,string:split(lists:nth(1,string:split(atom_to_list(Name),"@")), "s")),
             
             io:format("Init ~n"),
+            GMType = {state_pair, [state_lwwregister, state_lwwregister]},
+            GMName = "GlobalMean",
+            GMSet = {GMName, GMType},
+            {ok, {GlobalMean, _, _, _}} = lasp:declare(GMSet, GMType),
             Buffer = declare_loop([], NumberOfNodes, 1),
 
             %[grisp_led:color(L, red) || L <- LEDs],
@@ -61,7 +65,7 @@ start(_StartType , _StartArgs) ->
 	    io:format("My Id is : ~p ~n", [Id]),
 	    io:format("Buffer of distributed variables is : ~p ~n", [Buffer]),
             io:format("Launching the daemon ~n"),
-            daemon(10000, Id, Buffer, NumberOfNodes),
+            daemon(5000, Id, Buffer, NumberOfNodes, GlobalMean),
             {ok , Pid};
         Error ->
             {error, Error}
@@ -84,26 +88,24 @@ stop(_State) ->
 %%% Internal functions
 %%%===================================================================
 
-daemon(Sleep, Id, Buffer, NumberOfNodes) -> 
-    io:format("DAAAAEEEMON ~n"),
+daemon(Sleep, Id, Buffer, NumberOfNodes, GlobalMean) -> 
+    io:format("__New round__~n"),
     
-    Temperature = rand:uniform(20)+12, %Mesure de la temperature
+    Temperature = rand:uniform(7)+15, %Mesure de la temperature
     TemperatureStr = integer_to_list(Temperature),
     MyNode = lists:nth(list_to_integer(Id),Buffer),
     %io:format("La valeur  ~p ~n", [Value]),
     {ok, {TT, _, _, _}} = lasp:update(MyNode, {fst, {set, timestamp(), TemperatureStr}}, self()),
-    {ok, {T, _, _, _}} = lasp:update(TT, {snd, {set, timestamp(), timestamp()}}, self()),
-    {ok, TRes} = lasp:query(T),    
+    lasp:update(TT, {snd, {set, timestamp(), timestamp()}}, self()),
     io:format("Temperature I measured : ~p ~n", [Temperature]),
-    io:format("Temperature I have put in lasp variable : ~p ~n", [TRes]), 
 
-    
-    timer:sleep(Sleep), 
-    Mean = mean_compute(Buffer, NumberOfNodes, 0, 1),
-    io:format("Average temperature I computed : ~p ~n", [Mean]),
+    FirstSleep = round(Sleep/3),
+    timer:sleep(FirstSleep), 
+    Mean = mean_compute(Sleep, Id, GlobalMean, Buffer, NumberOfNodes, 0, 1, 0, 0),
+    io:format("---Average temperature I computed : ~p ---~n", [Mean]),
 
     timer:sleep(Sleep),
-    daemon(Sleep, Id, Buffer, NumberOfNodes).
+    daemon(Sleep, Id, Buffer, NumberOfNodes, GlobalMean).
 
 
 timestamp() ->
@@ -122,22 +124,43 @@ declare_loop(L, NumberOfNodesToDeclare, Counter) when NumberOfNodesToDeclare > 0
     declare_loop(erlang:append(L,[T]), NumberOfNodesToDeclare-1, Counter+1).
 
 
-mean_compute(Buffer, NumberOfNodes, Mean, Counter) when Counter > NumberOfNodes ->
-    io:format("The Buffer is : ~p ~n", [Buffer]),
-    io:format("The mean is : ~p ~n", [Mean]),
-    Mean;
-mean_compute(Buffer, NumberOfNodes, Mean, Counter) when Counter =< NumberOfNodes ->
+mean_compute(Sleep, Id, GlobalMean, Buffer, NumberOfNodes, Mean, Counter, CounterValid, Chef) when Counter > NumberOfNodes ->
+    io:format("The Number of Valid node is : ~p ~n", [CounterValid]),
+    Return = round(Mean/CounterValid),
+    ReturnStr = integer_to_list(Return),
+    if (Chef == Id) or (Chef == 0) ->
+    io:format("I AM THE CHEF"),
+    {ok, {Global, _, _, _}} = lasp:update(GlobalMean, {fst, {set, timestamp(), ReturnStr}}, self()),
+    lasp:update(Global, {snd, {set, timestamp(), timestamp()}}, self()),
+    Return;
+    true ->
+        Return
+    end;
+mean_compute(Sleep, Id, GlobalMean, Buffer, NumberOfNodes, Mean, Counter, CounterValid, Chef) when Counter =< NumberOfNodes ->
     Node = lists:nth(Counter,Buffer),
-    io:format("Lasp variable I want to query : ~p ~n", [Node]),
+    io:format("Lasp variable I want to query : ~p ~n", [erlang:element(1,Node)]),
     {ok, Tuple} = lasp:query(Node),
-    case Tuple of 
-	undefined -> io:format("Bug: I got an undefined temperature from the query ~n"),
-		     NewMean = 0;
-	Value ->  RemoteTemperature = erlang:element(1,Tuple),
-		 io:format("I got the temperature ~p from the query ~n", [RemoteTemperature]),
-		 NewMean = Mean+(list_to_integer(RemoteTemperature)/NumberOfNodes) end
-
+    case erlang:element(1,Tuple) of 
+	undefined -> 
+        io:format("--- ERROR: I got an undefined temperature from the query ~n"),
+        NewCounterValid = CounterValid,
+        NewChef = Chef,
+        NewMean = Mean;
+	Value ->
+        Time_diff = timestamp() - erlang:element(2,Tuple),
+        io:format("UnixTime : ~p ~n", [Time_diff]),
+        if (Chef == 0) and (Time_diff < (Sleep*3)) ->
+            NewChef = list_to_integer(lists:nth(2,string:split(erlang:element(1,Node),"T"))),
+            io:format("We found the chef :  ~p ~n ", [NewChef]);
+        true ->
+            NewChef = Chef
+        end,
+        RemoteTemperature = Value,
+        NewCounterValid = CounterValid + 1,
+		io:format("I got the temperature ~p from the query ~n", [RemoteTemperature]),
+		NewMean = Mean+(list_to_integer(RemoteTemperature)) 
+    end,
     %Value = erlang:element(1,Tuple),
     %io:format("Temperature I got from this query : ~p ~n", [Value]),
     %NewMean = Mean+(list_to_integer(Value)/NumberOfNodes),
-    mean_compute(Buffer, NumberOfNodes, NewMean, Counter+1).
+    mean_compute(Sleep, Id, GlobalMean, Buffer, NumberOfNodes, NewMean, Counter+1, NewCounterValid, NewChef).
